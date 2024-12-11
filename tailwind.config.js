0acb7096bce9f70cf3c27b89b30bcd754fd4613e
nocheol <qkdl60@gmail.com>
2024-12-11 14:46:52 +0900

    tailwindcss 추가

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import colors from "./styles/colors.js";
+import fontSize from "./styles/fontSize.js";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors,
+      fontSize,
+    },
+  },
+  plugins: [],
+};
